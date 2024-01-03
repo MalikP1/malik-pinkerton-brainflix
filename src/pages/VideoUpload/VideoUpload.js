ec89bd7c444fba616f747af21bc65ab375c33570
MalikP1 <m.pinkerton7@outlook.com>
2024-01-03 00:18:44 +0000

    add upload image, add all contents for upload page with styling and positioning

--- src/pages/VideoUpload/VideoUpload.js
+++ src/pages/VideoUpload/VideoUpload.js
@@ -1,9 +1,42 @@
+import videoPreview from "../../assets/images/Upload-video-preview.jpg";
+import "./VideoUpload.scss";
+
 function VideoUpload() {
   return (
-    <main className="video">
-      <h1 className="video__title">Upload Video</h1>
-      <label>VIDEO THUMBNAIL</label>
-    </main>
+    <div className="max-width-upload">
+      <main className="video">
+        <h1 className="video__title">Upload Video</h1>
+        <form className="video__form">
+          <section className="main-content">
+            <article className="image-container">
+              <label className="video__label">VIDEO THUMBNAIL</label>
+              <img
+                className="video__image"
+                src={videoPreview}
+                alt="person cycling on light blue bike"
+              />
+            </article>
+            <article className="input-container">
+              <label className="video__label">TITLE YOUR VIDEO</label>
+              <input
+                className="video__input"
+                placeholder="Add a title to your video"
+              />
+              <label className="video__label">ADD A VIDEO DESCRIPTION</label>
+              <textarea
+                className="video__text"
+                placeholder="Add a description to your video"
+              />
+            </article>
+          </section>
+
+          <article className="button">
+            <button className="button__cancel">CANCEL</button>
+            <button className="button__publish">PUBLISH</button>
+          </article>
+        </form>
+      </main>
+    </div>
   );
 }
 
