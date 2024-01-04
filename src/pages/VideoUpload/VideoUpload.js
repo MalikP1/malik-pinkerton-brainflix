56aa62dfae0d183a0f705809854ef1d91a7a5043
MalikP1 <m.pinkerton7@outlook.com>
2024-01-04 17:26:22 +0000

    change video upload class names and change video sass

--- src/pages/VideoUpload/VideoUpload.js
+++ src/pages/VideoUpload/VideoUpload.js
@@ -1,6 +1,7 @@
 import videoPreview from "../../assets/images/Upload-video-preview.jpg";
 import "./VideoUpload.scss";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 function VideoUpload() {
   return (
@@ -10,24 +11,31 @@ function VideoUpload() {
         <form className="upload__form">
           <section className="main-content">
             <article className="image-container">
-              <label className="video__label">VIDEO THUMBNAIL</label>
+              <label className="upload__label">VIDEO THUMBNAIL</label>
               <img
-                className="video__image"
+                className="upload__image"
                 src={videoPreview}
                 alt="person cycling on light blue bike"
               />
             </article>
             <article className="input-container">
-              <label className="video__label">TITLE YOUR VIDEO</label>
-              <input
-                className="video__input"
-                placeholder="Add a title to your video"
-              />
-              <label className="video__label">ADD A VIDEO DESCRIPTION</label>
-              <textarea
-                className="video__text"
-                placeholder="Add a description to your video"
-              />
+              <label className="upload__label">
+                TITLE YOUR VIDEO
+                <input
+                  className="upload__input"
+                  placeholder="Add a title to your video"
+                  name="input"
+                />
+              </label>
+
+              <label className="upload__label">
+                ADD A VIDEO DESCRIPTION
+                <textarea
+                  className="upload__text"
+                  placeholder="Add a description to your video"
+                  name="textArea"
+                />
+              </label>
             </article>
           </section>
 
